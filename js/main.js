29056f1ca038f519a2d8ba3e13d507182d6a55cd
Maksim Galimov <maksimgalimov@gmail.com>
2021-03-21 10:24:33 +0100

    and one more commit

--- js/main.js
+++ js/main.js
@@ -6,12 +6,12 @@ logo = document.querySelector('.navigation__logo')
 button.addEventListener("click", function() {
     
     menu.classList.remove("menu");
-    menu.classList.add("open");
+    menu.classList.add("open", "fade");
     
     if (
         menu.classList.item(0) == "open"
     ) {
-        menu.classList.remove("open")
+        menu.classList.remove("open", "fade")
         menu.classList.add("hidden");
     } else {
         
